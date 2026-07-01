cfa9e05bf69794d5a48c1dbd4ae375a1c34a5cd6
izadyar-amirreza <133513684+izadyar-amirreza@users.noreply.github.com>
2026-07-01 17:50:56 +0330

    fix: add trustProxies to bootstrap/app.php for Render HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,12 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        
+        // این خط اضافه شد تا پروکسی‌های Render را معتبر بشناسد و مشکل HTTPS حل شود
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
